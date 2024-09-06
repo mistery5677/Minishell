0acdf99642e78399e800d96afbdf06d9c2322a59
mfrancis <margarida.matiass@gmail.com>
2024-09-06 16:11:02 +0100

    teste

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: mfrancis <mfrancis@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/05 21:42:32 by mfrancis          #+#    #+#             */
-/*   Updated: 2024/09/06 16:00:52 by mfrancis         ###   ########.fr       */
+/*   Updated: 2024/09/06 16:10:51 by mfrancis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ int main(int argc, char *argv[], char *env[])
       
         
     }
+    printf("teste\n");
     return (0);
 }
 
@@ -27,7 +28,7 @@ int main(int argc, char *argv[], char *env[])
         [] Inicialization of data/ minishell;
         [] infinit loop
             [] tratamento dos sinais - interactive mode;
-            [] displauy_prompt - minishell$
+            [] display_prompt - minishell$
             [] ler input  - GNL ou realine
                 [] token
                 [] Parsing
